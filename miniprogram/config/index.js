dd604a2f9442a3d1da23b3e73efaa852094a021c
batare11 <batare11@users.noreply.github.com>
2026-06-20 14:09:20 +0800

    chore: configure local API for testing

--- miniprogram/config/index.js
+++ miniprogram/config/index.js
@@ -3,7 +3,10 @@
  * 本地调试可改为 http://127.0.0.1:5000/api/v1，
  * 真机调试需使用电脑局域网 IP，不能使用 127.0.0.1。
  */
-const BASE_URL = "https://hiothy.cn/api/v1";
+// 正式上线域名配置
+// const BASE_URL = "https://hiothy.cn/api/v1";
+// 测试正式ip
+const BASE_URL = "http://127.0.0.1:5000/api/v1";
 
 module.exports = {
   BASE_URL,
